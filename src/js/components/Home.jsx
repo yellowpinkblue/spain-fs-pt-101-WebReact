03502833096c03fe7d1d0fb176fb2dd19532692a
Alice <alicefontes@hotmail.com>
2025-04-05 20:14:05 +0000

    segunda entrega

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./navbar";
 import Jumbotron from "./jumbotron";
-import Card from "./card";
+import {MyCard} from "./mycard";
 import Footer from "./footer";
 
 //include images into your bundle
@@ -10,26 +10,46 @@ import Footer from "./footer";
 
 const Home = () => {
 	return (
-		<div classNameName="text-center">
+		<div className="text-center">
 			<Navbar />
 			<Jumbotron />
 			<div className="container">
 				<div className="row">
 					<div className="col-sm-12 col-md-6 col-lg-3">
-						<Card />
+						<MyCard
+							imgUrl='https://cdn.pixabay.com/photo/2025/02/20/10/38/robin-9419575_1280.jpg'
+							title='Robin Bird'
+							text='Bird on the nature'
+							btn='Click on me!'
+						/>
 					</div>
 					<div className="col-sm-12 col-md-6 col-lg-3">
-						<Card />
+						<MyCard
+							imgUrl='https://cdn.pixabay.com/photo/2022/07/25/18/47/waterfall-7344396_1280.jpg'
+							title='Mountains and flowers'
+							text='Enjoy, breathe fresh air'
+							btn='Press me!'
+						/>
 					</div>
 					<div className="col-sm-12 col-md-6 col-lg-3">
-						<Card />
+						<MyCard
+							imgUrl='https://cdn.pixabay.com/photo/2023/10/16/06/21/white-butterfly-8318526_1280.jpg'
+							title='Butterfly'
+							text='White butterfly on lavender'
+							btn='Learn more!'
+						/>
 					</div>
 					<div className="col-sm-12 col-md-6 col-lg-3">
-						<Card />
+						<MyCard
+							imgUrl='https://cdn.pixabay.com/photo/2019/03/09/16/43/meerkat-4044441_1280.jpg'
+							title='Meerkat'
+							text='Meerkat average about 50cm. '
+							btn='Find out more!'
+						/>
 					</div>
 				</div>
 			</div>
-			<hr/>
+			<hr />
 
 			<Footer />
 		</div>
---
+++ src/js/components/mycard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export const MyCard = (props) => {
+    return (
+        <div className="card">
+            <img src={props.imgUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.text}</p>
+                <hr/>
+                <a href="a" className="btn btn-primary">{props.btn}</a>
+            </div>
+        </div>
+    );
+};
+
